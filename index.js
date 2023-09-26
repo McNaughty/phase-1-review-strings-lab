b78c6c93687f1d55c3bdfd7d108ecafb4d3d385a
Winnie <wimacoj@gmail.com>
2023-09-26 11:18:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+
+
+const currentUser = "Wabuor"
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//console.log(welcomeMessage.toUpperCase);
+
+//console.log(currentUser.length);
+
+let currUser =currentUser.slice(0,1);
+
+let shortGreeting = `Welcome, ${currUser}!`;
+
+console.log(currUser);
